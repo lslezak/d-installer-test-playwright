060d69d660c274f46a19140cec5ca281ab367d05
Ladislav Slezák <lslezak@suse.cz>
2022-12-07 19:51:38 +0100

    Initial version

---
+++ global-setup.ts
@@ -0,0 +1,26 @@
+// See https://playwright.dev/docs/auth#reuse-signed-in-state
+import { expect, chromium, FullConfig } from '@playwright/test';
+
+async function globalSetup(config: FullConfig) {
+  const browser = await chromium.launch();
+  const page = await browser.newPage({
+    baseURL: process.env.BASE_URL || 'http://localhost:9090',
+    ignoreHTTPSErrors: true
+  });
+
+  await page.goto('/');
+
+  await page.getByLabel('User name').fill('root');
+  await page.getByLabel('Password').fill('linux');
+
+  await page.getByRole('button', { name: 'Log in' }).click();
+
+  await expect(page.getByLabel('Password')).toHaveCount(0);
+
+  // Save signed-in state to 'storageState.json'.
+  await page.context().storageState({ path: 'storageState.json' });
+
+  await browser.close();
+}
+
+export default globalSetup;
